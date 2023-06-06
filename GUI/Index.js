5a360b36be032250a5c4c9d9c6af491c1fc43268
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2023-06-06 10:40:39 +0200

    fix

--- GUI/Index.js
+++ GUI/Index.js
@@ -7,8 +7,22 @@ element4 =    document.getElementById('1.2')
 element5 =    document.getElementById('0.4'),
 element6 =    document.getElementById('1.1')
 
-new LeaderLine(element1, element2, {dash: {animation: true}, color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
-new LeaderLine(element3, element4, {dash: {animation: true}, color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
-new LeaderLine(element5, element6, {dash: {animation: true}, color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
 
+element11 =    document.getElementById('2.1'),
+element21 =    document.getElementById('3.3')
+
+element31 =    document.getElementById('2.2'),
+element41 =    document.getElementById('3.0')
+
+element51 =    document.getElementById('2.4'),
+element61 =    document.getElementById('3.2')
+
+new LeaderLine(element1, element2, {color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
+new LeaderLine(element3, element4, {color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
+new LeaderLine(element5, element6, { color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
+
+
+new LeaderLine(element11, element21, {color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
+new LeaderLine(element31, element41, {color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
+new LeaderLine(element51, element61, { color: 'red', startSocket: 'bottom', endSocket: 'top', path : 'straight'});
 
